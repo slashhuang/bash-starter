3acefe579608454c6615880f7d1a44954fbad3fe
slashhuang <627284482@qq.com>
2017-10-15 22:08:41 +0800

    revise tutorial for bash

--- tutorial/npminit.sh
+++ tutorial/npminit.sh
@@ -2,20 +2,32 @@
 
 
 hint="欢迎使用npm init"
+inputName="input name:"
+version="input version 1.0.0:"
+
 result=""
 echo $hint
-echo "input name:"
+
+# 名字
+echo $inputName
 read name
-echo "nput name: is ${name} \n"
-echo "input version 1.0.0:"
+echo "$inputName ${name} \n"
+result="${result} + $inputName ${name}"
+
+# 版本号
+echo "$version"
 read version
-echo "nput version: is ${version} \n"
+
+echo "$version:  ${version} \n"
+result="${result} + $version:  ${version}"
+
+# 是否ok
 echo "这样ok吗? 1为yes，其他为no"
 read yesNo
 if [ $yesNo -eq 1 ]
 then
     echo "${result}" > package.json
 else 
-    echo "sorry,goodbyg"
+    echo "sorry,goodbye"
 fi
 
